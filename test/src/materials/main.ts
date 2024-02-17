ff3955f77b4b35d7fee9f1f0f97f67f562741d8b
gavinyork <gavinyork2024@outlook.com>
2024-02-18 01:15:47 +0800

    Add material test case

---
+++ test/src/materials/main.ts
@@ -0,0 +1,70 @@
+import { Vector3, Vector4 } from '@zephyr3d/base';
+import {
+  Scene,
+  Application,
+  OrbitCameraController,
+  PerspectiveCamera,
+  Compositor,
+  Tonemap,
+  DirectionalLight,
+  Mesh,
+  LambertMaterial,
+  SphereShape
+} from '@zephyr3d/scene';
+import { imGuiEndFrame, imGuiInit, imGuiInjectEvent, imGuiNewFrame } from '@zephyr3d/imgui';
+import * as common from '../common';
+import { WoodMaterial } from './materials/wood';
+
+const myApp = new Application({
+  backend: common.getBackend(),
+  canvas: document.querySelector('#canvas')
+});
+
+myApp.ready().then(async function () {
+  await imGuiInit(myApp.device);
+
+  const scene = new Scene();
+
+  let dlight: DirectionalLight = null;
+  // Create directional light
+  dlight = new DirectionalLight(scene);
+  // light direction
+  dlight.rotation.fromEulerAngle(-Math.PI / 4, Math.PI / 4, 0, 'ZYX');
+  // light color
+  dlight.color = new Vector4(1, 1, 1, 1);
+
+  // Create sphere
+  const sphereMaterial = new WoodMaterial();
+  new Mesh(scene, new SphereShape({ radius: 2 }), sphereMaterial);
+
+  // Create camera
+  const camera = new PerspectiveCamera(
+    scene,
+    Math.PI / 3,
+    myApp.device.canvas.width / myApp.device.canvas.height,
+    1,
+    600
+  );
+  camera.lookAt(new Vector3(0, 0, 12), new Vector3(0, 0, 0), new Vector3(0, 1, 0));
+  camera.controller = new OrbitCameraController({ distance: 12 });
+
+  const compositor = new Compositor();
+  // Add a Tonemap post-processing effect
+  compositor.appendPostEffect(new Tonemap());
+
+  const inspector = new common.Inspector(scene, compositor, camera);
+
+  myApp.inputManager.use(imGuiInjectEvent);
+  myApp.inputManager.use(camera.handleEvent.bind(camera));
+
+  myApp.on('tick', function () {
+    camera.updateController();
+    camera.render(scene, compositor);
+
+    imGuiNewFrame();
+    inspector.render();
+    imGuiEndFrame();
+  });
+
+  myApp.run();
+});
