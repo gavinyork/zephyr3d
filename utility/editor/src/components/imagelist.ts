c0fbfe8d77ad0b43870003b9f5b53b9e63835ac5
gavinyork <gavinyork2024@outlook.com>
2025-03-25 18:58:51 +0800

    WIP: editor

---
+++ utility/editor/src/components/imagelist.ts
@@ -0,0 +1,149 @@
+import { Texture2D } from '@zephyr3d/device';
+import { ImGui } from '@zephyr3d/imgui';
+import { DRef } from '@zephyr3d/scene';
+
+type ImageInfo = {
+  texture: DRef<Texture2D>;
+  selected: boolean;
+};
+export class ImageList {
+  private _images: ImageInfo[];
+  private _isDragging: boolean;
+  private _lastMouseX: number;
+  private _selectedIndex: number;
+  private _spacing: number;
+  constructor() {
+    this._images = [];
+    this._isDragging = false;
+    this._lastMouseX = 0;
+    this._selectedIndex = -1;
+    this._spacing = 5;
+  }
+  private calcTotalWidth(height: number): number {
+    let totalWidth = 0;
+    for (const img of this._images) {
+      totalWidth += (height * img.texture.get().width) / img.texture.get().height + this._spacing;
+    }
+    return totalWidth;
+  }
+  addImage(img: Texture2D) {
+    this._images.push({
+      texture: new DRef(img),
+      selected: false
+    });
+  }
+  clear() {
+    for (const img of this._images) {
+      img.texture.dispose();
+    }
+    this._images = [];
+  }
+  scrollToSelected(height: number) {
+    if (this._selectedIndex >= 0 && this._selectedIndex < this._images.length) {
+      let offsetX = 0;
+      const availableHeight = height;
+      for (let i = 0; i < this._selectedIndex; i++) {
+        const tex = this._images[i].texture.get();
+        offsetX += availableHeight * (tex.width / tex.height) + this._spacing;
+      }
+      const currentTex = this._images[this._selectedIndex].texture.get();
+      const windowWidth = ImGui.GetWindowWidth();
+      const imgWidth = availableHeight * (currentTex.width / currentTex.height);
+      const centeredOffset = offsetX - (windowWidth - imgWidth) * 0.5;
+      ImGui.SetScrollX(Math.max(centeredOffset, 0));
+    }
+  }
+  get selected() {
+    return this._selectedIndex;
+  }
+  set selected(index: number) {
+    if (index >= 0 && index < this._images.length) {
+      this._selectedIndex = index;
+      for (let i = 0; i < this._images.length; i++) {
+        this._images[i].selected = i === index;
+      }
+    }
+  }
+  render(size: ImGui.ImVec2) {
+    ImGui.PushID(`${ImGui.GetCurrentWindow().ID}`);
+    let selectionChanged = false;
+    const availableWidth = size.x;
+    const availableHeight = size.y;
+    const totalHeight = availableHeight - ImGui.GetStyle().ScrollbarSize - 2 * this._spacing;
+    const totalWidth = this.calcTotalWidth(totalHeight);
+    const allowDragging = totalWidth > availableWidth;
+    let flags = ImGui.WindowFlags.AlwaysHorizontalScrollbar;
+    if (!allowDragging) {
+      flags |= ImGui.WindowFlags.NoScrollWithMouse;
+    }
+    ImGui.BeginChild('ImageList', size, true, flags);
+    if (allowDragging) {
+      ImGui.Dummy(new ImGui.ImVec2(totalWidth - this._spacing, 1));
+      ImGui.SetCursorPos(new ImGui.ImVec2(0, 0));
+    }
+    if (allowDragging && ImGui.IsWindowHovered() && ImGui.IsMouseDown(ImGui.MouseButton.Left)) {
+      if (!this._isDragging) {
+        this._isDragging = true;
+        this._lastMouseX = ImGui.GetIO().MousePos.x;
+      } else {
+        const deltaX = ImGui.GetIO().MousePos.x - this._lastMouseX;
+        ImGui.SetScrollX(ImGui.GetScrollX() - deltaX);
+        this._lastMouseX = ImGui.GetIO().MousePos.x;
+      }
+    } else {
+      this._isDragging = false;
+    }
+    const scrollX = ImGui.GetScrollX();
+    const visibleStartX = scrollX;
+    const visibleEndX = scrollX + availableWidth;
+    let cursorX = this._spacing;
+    for (let i = 0; i < this._images.length; i++) {
+      ImGui.PushID(i);
+      const img = this._images[i];
+      const tex = img.texture.get();
+      const imgWidth = totalHeight * (tex.width / tex.height);
+      ImGui.SetCursorPosX(cursorX);
+      ImGui.SetCursorPosY(this._spacing);
+      cursorX += imgWidth + this._spacing;
+      let b = false;
+      if (cursorX - this._spacing >= visibleStartX && cursorX - imgWidth - this._spacing <= visibleEndX) {
+        if (img.selected) {
+          b = true;
+          ImGui.PushStyleColor(ImGui.Col.Border, new ImGui.ImVec4(1.0, 0.5, 0.0, 1.0));
+          ImGui.PushStyleVar(ImGui.StyleVar.FrameBorderSize, 3);
+          ImGui.PushStyleVar(ImGui.StyleVar.FramePadding, new ImGui.ImVec2(3, 3));
+          ImGui.PushStyleColor(ImGui.Col.ButtonActive, new ImGui.ImVec4(0.8, 0.8, 0.8, 0.5));
+          ImGui.PushStyleColor(ImGui.Col.ButtonHovered, new ImGui.ImVec4(0.7, 0.7, 0.7, 0.5));
+        }
+        if (
+          ImGui.ImageButton(
+            tex,
+            new ImGui.ImVec2(imgWidth, totalHeight),
+            new ImGui.ImVec2(0, 0),
+            new ImGui.ImVec2(1, 1),
+            0
+          )
+        ) {
+          if (this._selectedIndex !== i) {
+            this._selectedIndex = i;
+            for (const img of this._images) {
+              img.selected = false;
+            }
+            this._images[i].selected = true;
+            selectionChanged = true;
+          }
+        }
+        if (b) {
+          ImGui.PopStyleColor(3);
+          ImGui.PopStyleVar(2);
+        }
+      } else {
+        ImGui.Dummy(new ImGui.ImVec2(imgWidth, totalHeight));
+      }
+      ImGui.PopID();
+    }
+    ImGui.EndChild();
+    ImGui.PopID();
+    return selectionChanged;
+  }
+}
--- utility/editor/src/views/edittools/terrain.ts
+++ utility/editor/src/views/edittools/terrain.ts
@@ -1,15 +1,23 @@
-import { ClipmapTerrain, DRef } from '@zephyr3d/scene';
+import { Application, ClipmapTerrain, DRef } from '@zephyr3d/scene';
 import type { EditTool } from './edittool';
 import type { Vector3 } from '@zephyr3d/base';
 import type { MenuItemOptions } from '../../components/menubar';
 import type { ToolBarItem } from '../../components/toolbar';
+import { ImGui } from '@zephyr3d/imgui';
+import { ImageList } from '../../components/imagelist';
 
 export class TerrainEditTool implements EditTool {
   private _terrain: DRef<ClipmapTerrain>;
   private _disposed: boolean;
+  private _brushImageList: ImageList;
   constructor(terrain: ClipmapTerrain) {
     this._terrain = new DRef(terrain);
     this._disposed = false;
+    this._brushImageList = new ImageList();
+    for (let i = 0; i < 1; i++) {
+      this._brushImageList.addImage(Application.instance.device.createTexture2D('rgba8unorm', 1, 1));
+    }
+    this._brushImageList.selected = 0;
   }
   handlePointerEvent(evt: PointerEvent, hitObject: any, hitPos: Vector3): boolean {
     return false;
@@ -17,7 +25,14 @@ export class TerrainEditTool implements EditTool {
   handleKeyboardEvent(evt: KeyboardEvent): boolean {
     return false;
   }
-  render(): void {}
+  render(): void {
+    if (ImGui.Begin('Terrain Tools')) {
+      ImGui.BeginChild('BrushList', new ImGui.ImVec2(0, 60));
+      this._brushImageList.render(ImGui.GetContentRegionAvail());
+      ImGui.EndChild();
+    }
+    ImGui.End();
+  }
   getSubMenuItems(): MenuItemOptions[] {
     return [];
   }
