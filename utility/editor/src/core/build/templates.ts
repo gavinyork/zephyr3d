034fed30aee3163594ab2f5954db925dbee899cb
gavinyork <gavinyork2024@outlook.com>
2025-08-29 18:17:21 +0800

    WIP: minor changes

--- utility/editor/src/core/build/templates.ts
+++ utility/editor/src/core/build/templates.ts
@@ -1,16 +1,52 @@
 export const templateScript = `import type { IDisposable } from '@zephyr3d/base';
 import { RuntimeScript } from '@zephyr3d/runtime';
+
 // Change HostType to your attachment type
 type HostType = IDisposable;
-export default class MyScript extends RuntimeScript<HostType> {
+
+export default class extends RuntimeScript<HostType> {
+  /**
+   * Called exactly once right after the constructor.
+   * Use this for initialization that may be asynchronous (e.g., loading assets).
+   * You can return a Promise to delay subsequent lifecycle steps until initialization completes.
+   */
   onCreated(): void | Promise<void> {
   }
+
+  /**
+   * Called after onCreated() when this script is attached to a host object.
+   * You should store the attached host in your own member(s) for later use.
+   *
+   * If this script is implemented as a singleton, it may be attached to multiple hosts.
+   * In that case, onAttached() can be called multiple times; consider using an array
+   * (or a Set) to keep track of all attached hosts.
+   */
   onAttached(_host: HostType): void | Promise<void> {
   }
+
+  /**
+   * Called once per frame.
+   * Use this for per-frame updates such as animations, state changes, or logic.
+   *
+   * @param _deltaTime  Time elapsed since the previous frame (in seconds).
+   * @param _elapsedTime Total time since this script started running (in seconds).
+   */
   onUpdate(_deltaTime: number, _elapsedTime: number) {
   }
-  onDetached() {
+
+  /**
+   * Called when this script is detached from a specific host via Engine.detachScript(),
+   * or when that host is destroyed.
+   * Update your stored list of attached hosts here (e.g., remove the host).
+   */
+  onDetached(_host: HostType) {
   }
+
+  /**
+   * Called after all hosts have been detached from this script.
+   * The script instance will be discarded afterwards.
+   * Use this to clean up resources and free memory (dispose handles, cancel timers, remove listeners, etc.).
+   */
   onDestroy() {
   }
 }
